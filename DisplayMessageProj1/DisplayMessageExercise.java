a6f2caf8b6ef31abd757fbaa4b691d374ad61b0f
Robert Jones <robertdante@outlook.com>
2025-01-15 13:06:51 -0500

    initial commit

---
+++ DisplayMessageProj1/DisplayMessageExercise.java
@@ -0,0 +1,12 @@
+
+//initialize class
+public class DisplayMessageExercise {
+//	declare main method
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+	}
+
+}
