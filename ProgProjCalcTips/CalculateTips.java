25d41d32cf963d877fece0fb347e7192436714f2
Robert Jones <robertdante@outlook.com>
2025-01-19 14:28:02 -0500

    add structure to program

--- ProgProjCalcTips/CalculateTips.java
+++ ProgProjCalcTips/CalculateTips.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
+//Declare class 
 public class CalculateTips {
 
+//	declare main method 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+//		1. Get input from user (subtotal and gratuity)
+//		2. Calculate: convert gratuity rate to dollar amount (round 
+//		to two decimal points
+//		3. Calculate: add gratuity to subtotal
+//		4. Display final total and gratuity rate 
 
 	}
 
