9f4663c383e9a7d62ab8a54e577f027a4ca4ab81
Robert Jones <robertdante@outlook.com>
2025-01-19 17:32:08 -0500

    update calculation and display output

--- ProgProjInvestValue/CalculateInvestValue.java
+++ ProgProjInvestValue/CalculateInvestValue.java
@@ -11,21 +11,24 @@ public class CalculateInvestValue {
 		Scanner input = new Scanner(System.in);
 		
 		// Step 1: Get input from user: Investment amount, annual interest rate, years for investment)
-		System.out.println("Enter investment amount, for example 1000.56:");
+		System.out.print("Enter investment amount, for example 1000.56:");
 		double investmentAmount = input.nextDouble();
 		
-		System.out.println("Enter annual interest rate, for example 4.25:");
+		System.out.print("Enter annual interest rate, for example 4.25:");
 		double annualRate = input.nextDouble();
 		
-		System.out.println("Enter number of years, for example 1:");
+		System.out.print("Enter number of years, for example 1:");
 		int years = input.nextInt();
 		
 		// Step 2: Calculate monthly interest rate, converting percentage to decimal
-		double monthlyRate = annualRate / 12;
+		double monthlyRate = (annualRate / 100) / 12;
 		
 		// Step 3: Calculate investment value
 		double futureValue = investmentAmount * (Math.pow(1 + monthlyRate, 12 * years)); 
-		// Step 4: Display output (Interest amount, Interest Value, APR, years)
+		
+		// Step 4: Display output (Interest amount)
+		System.out.println("Accumulated value is $" + ((int) (futureValue * 100) / 100.0));
+		
 
 	}
 
