240d4d891833684e7e580ac5b28a47df1079c909
Robert Jones <robertdante@outlook.com>
2025-01-19 17:03:03 -0500

    initial commit

---
+++ ProgProjInvestValue/CalculateInvestValue.java
@@ -0,0 +1,20 @@
+/* Name: Robert
+Date: 1/19/2025
+Purpose: The purpose of this program is to calculate the future investment value from user's input. 
+Version: 1.0
+*/
+import java.util.Scanner;
+//declare class
+public class CalculateInvestValue {
+//declare main method
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Step 1: Get input from user: Investment amount, annual interest rate, years for investment)
+		// Step 2: Calculate monthly interest rate, converting percentage to decimal
+		// Step 3: Calculate investment value
+		// Step 4: Display output (Interest amount, Interest Value, APR, years)
+
+	}
+
+}
