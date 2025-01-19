7eb1cc8070a268c5f9beb484448577164c64f5d0
Robert Jones <robertdante@outlook.com>
2025-01-19 17:10:56 -0500

    declare variables

--- ProgProjInvestValue/CalculateInvestValue.java
+++ ProgProjInvestValue/CalculateInvestValue.java
@@ -11,6 +11,15 @@ public class CalculateInvestValue {
 		Scanner input = new Scanner(System.in);
 		
 		// Step 1: Get input from user: Investment amount, annual interest rate, years for investment)
+		System.out.println("Enter investment amount, for example 1000.56:");
+		double investmentAmount = input.nextDouble();
+		
+		System.out.println("Enter annual interest rate, for example 4.25:");
+		double annualRate = input.nextDouble();
+		
+		System.out.println("Enter number of years, for example 1:");
+		int years = input.nextInt();
+		
 		// Step 2: Calculate monthly interest rate, converting percentage to decimal
 		// Step 3: Calculate investment value
 		// Step 4: Display output (Interest amount, Interest Value, APR, years)
