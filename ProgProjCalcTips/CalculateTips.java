1df3f3ce92fd76a9e6dd83838eb781b10c3813ca
Robert Jones <robertdante@outlook.com>
2025-01-19 14:33:36 -0500

    declare and init subtotal and gratuity

--- ProgProjCalcTips/CalculateTips.java
+++ ProgProjCalcTips/CalculateTips.java
@@ -8,8 +8,14 @@ public class CalculateTips {
 		Scanner input = new Scanner(System.in);
 		
 //		1. Get input from user (subtotal and gratuity)
+		System.out.print("Enter the subtotal and a gratuity rate. for example 10 15:");
+		double subtotal = input.nextDouble();
+		double gratuity = input.nextDouble();
+		
 //		2. Calculate: convert gratuity rate to dollar amount (round 
 //		to two decimal points
+		
+		
 //		3. Calculate: add gratuity to subtotal
 //		4. Display final total and gratuity rate 
 
