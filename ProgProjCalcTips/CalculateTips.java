76f76055b2259892000d0310cc88b8545ee1aa9c
Robert Jones <robertdante@outlook.com>
2025-01-19 16:04:29 -0500

    separate inputs, comment updates, declared variable, int casting

--- ProgProjCalcTips/CalculateTips.java
+++ ProgProjCalcTips/CalculateTips.java
@@ -8,18 +8,23 @@ public class CalculateTips {
 		Scanner input = new Scanner(System.in);
 		
 //		1. Get input from user (subtotal and gratuity)
-		System.out.print("Enter the subtotal and a gratuity rate. for example 10 15:");
+		System.out.print("Enter the subtotal, for example 10:");
 		double subtotal = input.nextDouble();
+		System.out.print("Enter the gratuity, for example 15:");
 		double gratuity = input.nextDouble();
 		
-//		2. Calculate: convert gratuity rate to dollar amount (round 
-//		to two decimal points
-		double convertGratuity = gratuity / 100; 
+//		2. Calculate: convert gratuity rate to dollar amount
+		double convertGratuity = gratuity / 100.0; 
 		
 //		4. Calculate gratuity from percentage and subtotal
-		double finalTotal = subtotal * convertGratuity; 
+		double tipAmount = subtotal * convertGratuity; 
 		
-//		5. Display final total and gratuity rate 
+//		5. Calculate: add gratuity to subtotal
+		double finalTotal = tipAmount + subtotal; 
+		
+//		6. Display final total and gratuity rate (round to hundredth decimal point)
+		System.out.print("The gratuity is $" + ((int) (tipAmount * 100) / 100.0)  + " and total is $" + ((int) (finalTotal * 
+				100) / 100.0));
 	}
 
 }
