b14d498ec12f1996e2a12c6e84c40d80c045e7cd
Robert Jones <robertdante@outlook.com>
2025-01-20 18:30:59 -0500

    initial commit

---
+++ CalcDiscount/DiscountProject.java
@@ -0,0 +1,26 @@
+/*Name: Robert
+ * Purpose: The purpose of this program is to allow a user to
+ * calculate the total price of a product based on the discount. 
+ * Date: 1/20/2025
+ * Version: 1.0
+ */
+//import input
+import java.util.Scanner;
+//initialize class
+public class DiscountProject {
+//declare main method
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+//		1. Get input for discount rate and item price.
+		System.out.println("Enter the price of the item: $");
+		double itemPrice = input.nextDouble();
+		
+//		2. Convert discount percentage to decimal
+//		3. Calculate discount from item price
+//		4. Calculate and display final price. 
+		
+
+	}
+
+}
