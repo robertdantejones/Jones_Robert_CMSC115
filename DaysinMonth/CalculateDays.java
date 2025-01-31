7c514128911e2db6cec90feaea61680d33322f20
Robert Jones <robertdante@outlook.com>
2025-01-31 00:30:16 -0500

    initial commit

---
+++ DaysinMonth/CalculateDays.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CalculateDays {
+
+	public static void main(String[] args) {
+//		1. Gather input
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a month in the year (e.g., 1 for Jan):");
+		int month = input.nextInt();
+		
+		System.out.print("Enter a year:");
+		int year = input.nextInt();
+		
+		
+//		2. Convert input to month
+//		3. Calculate days 
+
+	}
+
+}
