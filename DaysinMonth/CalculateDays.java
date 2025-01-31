8a5e21d34d2836c57fe3359535f7516076d120b1
Robert Jones <robertdante@outlook.com>
2025-01-31 07:43:09 -0500

    calculate days

--- DaysinMonth/CalculateDays.java
+++ DaysinMonth/CalculateDays.java
@@ -30,6 +30,28 @@ public class CalculateDays {
 		default: System.out.print("Invalid Month"); break;
 		}
 //		3. Calculate days 
+		int days;
+		if (month == 1)
+			days = 31;
+		else if (month == 2)
+			days = 28;
+		else if (month == 3)
+			days = 31;
+		else if (month == 4)
+			days = 30;
+		else if (month == 5)
+			days = 31;
+		else if (month == 6)
+			days = 30;
+		else if (month >= 7 && month <= 10)
+			days = 31;
+		else if (month == 11)
+			days = 30;
+		else if (month == 12)
+			days = 31;
+		else 
+			System.out.print("Invalid Month");
+		
 		
 
 	}                                                            
