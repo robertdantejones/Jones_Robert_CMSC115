ebccd83936af45168592fea0f6c75c9de8822df5
Robert Jones <robertdante@outlook.com>
2025-02-08 12:25:37 -0500

    correcting area

--- PolygonGeometry/PolygonArea.java
+++ PolygonGeometry/PolygonArea.java
@@ -17,7 +17,7 @@ public class PolygonArea {
 		double length = input.nextDouble();
 		
 //		Step 2. Compute Area of Polygon
-		double area = (sides * Math.pow(sides, 2) / (4 * Math.tan(Math.PI / length)));
+		double area = (sides * Math.pow(length, 2) / (4 * Math.tan(Math.PI / length)));
 		
 //		Step 3. Display area and user inputs
 		System.out.print("The area of a polygon is " + area);
