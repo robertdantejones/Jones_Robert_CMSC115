f17e867eb5b3d9d18d5d02950093cd2e4d5f5d06
Robert Jones <robertdante@outlook.com>
2025-02-08 12:16:12 -0500

    structure

--- PolygonGeometry/PolygonArea.java
+++ PolygonGeometry/PolygonArea.java
@@ -7,6 +7,10 @@ public class PolygonArea {
 //	declaring main method
 	public static void main(String[] args) {
 		
+//		Step 1. Gather User Input for Sides and Length of Polygon
+//		Step 2. Compute Area of Polygon
+//		Step 3. Display area and user inputs
+		
 
 	}
 
