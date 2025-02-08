0ef8a79ebf7552940c3b8003301a6a0067bc9325
Robert Jones <robertdante@outlook.com>
2025-02-08 14:36:10 -0500

    corrections and final commmit

--- CheckSSN/ValidateSSN.java
+++ CheckSSN/ValidateSSN.java
@@ -8,9 +8,21 @@ public class ValidateSSN {
 		Scanner input = new Scanner(System.in);
 		
 //		Step 1. Gather user input
-//		Step 2. Declare Variables 
-//		Check if string is digits and display output
+		System.out.print("Enter a string for SSN: ");
+		String ssn = input.nextLine();
 
+//		Step 2. Extract substrings from dashes
+		String firstSet = ssn.substring(0, 3);
+		String secondSet = ssn.substring(4, 6);
+		String thirdSet = ssn.substring(7);
+		
+//		Step 2. Check if input is valid and display output
+		if ((ssn.length() > 11 || ssn.length() < 11) || (firstSet.contains("-") || 
+				secondSet.contains("-") || thirdSet.contains("-")))
+			System.out.print(ssn + " is an invalid social security number");
+		else 
+			System.out.print(ssn + " is an valid social security number");
+		
 	}
 
 }
