30db182ef40bb2f73cb3a73edc7d3eb290013bb7
Robert Jones <robertdante@outlook.com>
2025-02-08 13:05:02 -0500

    construct

--- CheckSSN/ValidateSSN.java
+++ CheckSSN/ValidateSSN.java
@@ -7,7 +7,9 @@ public class ValidateSSN {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		
+//		Step 1. Gather user input
+//		Step 2. Declare Variables 
+//		Check if string is digits and display output
 
 	}
 
