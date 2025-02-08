368e53898c92943da8763f70a77f2863f4338947
Robert Jones <robertdante@outlook.com>
2025-02-07 20:57:00 -0500

    initial commit

---
+++ CalculateRectangle/RectangleValues.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RectangleValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Rectangle {
+	
+	
+}
