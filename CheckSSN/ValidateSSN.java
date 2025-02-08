c4b0d676b239aeb02a33538ae5e89b03407ae3d9
Robert Jones <robertdante@outlook.com>
2025-02-08 13:02:40 -0500

    initial commit

---
+++ CheckSSN/ValidateSSN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//declare class
+public class ValidateSSN {
+
+//	declare method 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+
+	}
+
+}
