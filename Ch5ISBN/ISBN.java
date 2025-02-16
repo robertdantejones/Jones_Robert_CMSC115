8c2dbe4131e696076fd4592575fe53d780c8af26
Robert Jones <robertdante@outlook.com>
2025-02-16 17:36:52 -0500

    initial commit

---
+++ Ch5ISBN/ISBN.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ISBN {
+
+	public static void main(String[] args) {
+		// Step 1: Gather input
+		// Step 2: Calculate 13 digit (checksum)
+		// Step 3: Display output
+
+	}
+
+}
